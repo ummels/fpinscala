2ae36b8abc8c409eddea229b2e8deb39993d6c21
Michael Ummels <michael@ummels.de>
2015-05-03 19:05:30 +0200

    Fix warning

--- exercises/src/main/scala/fpinscala/parallelism/Nonblocking.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Nonblocking.scala
@@ -2,6 +2,7 @@ package fpinscala.parallelism
 
 import java.util.concurrent.{Callable, CountDownLatch, ExecutorService}
 import java.util.concurrent.atomic.AtomicReference
+import scala.language.implicitConversions
 
 object Nonblocking {
 
